e0007bfbe2bbbefb939a2873528bafb05072e0a8
Ptuyizere <p.tuyizere1@alustudent.com>
2025-05-29 12:05:02 +0200

    Added 1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -3,7 +3,7 @@
 input = ARGV[0]
 
 # Match pattern: h, b, at least two 't's, then n
-if input =~ /^hbtt{2,5}n$/
+if input =~ /^(hbttn|hbtttn|hbttttn|hbtttttn)$/
   puts "Match found!"
 else
   puts "No match."
